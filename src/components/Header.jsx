261e7fdd665cd2f339df500fb9970a90d7dad521
HuynhAnh24h <anhdev24h@gmail.com>
2025-04-18 13:07:25 +0700

    setting src done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,30 @@
 import React, { useState } from 'react';
+import { FaXTwitter, FaInstagram, FaLinkedin, FaFacebookF } from "react-icons/fa6";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <header className="bg-[#121212] text-white px-4 py-3">
-      <div className="max-w-7xl mx-auto flex items-center justify-between rounded-2xl border border-[#e72323de] px-6 py-3 bg-[#1a1a1a]">
+      <div className="max-w-7xl mx-auto flex items-center justify-between rounded-2xl border border-[#14f195] px-6 py-3 bg-[#1a1a1a]">
         {/* Logo */}
         <div className="flex items-center space-x-2 text-xl font-semibold">
-          <span className="text-[#e72323de]">&lt;<span >Otobi.<span className="text-gray-300"> Dev</span></span> /&gt;</span>
+          <span className="text-[#14f195]">&lt;<span >Otobi.<span className="text-gray-300"> Dev</span></span> /&gt;</span>
         </div>
 
         {/* Desktop Menu */}
         <nav className="hidden md:flex items-center space-x-8 text-lg">
-          <a href="#about" className="hover:text-[#e72323de] transition">About Me</a>
-          <a href="#resume" className="hover:text-[#e72323de] transition">Resume</a>
-          <a href="#contact" className="hover:text-[#e72323de] transition">Contacts</a>
+          <a href="#about" className="hover:text-[#14f195] transition">About Me</a>
+          <a href="#resume" className="hover:text-[#14f195] transition">Resume</a>
+          <a href="#contact" className="hover:text-[#14f195] transition">Contacts</a>
         </nav>
 
         {/* Social Icons */}
         <div className="hidden md:flex items-center space-x-4">
-
+            <FaXTwitter className="hover:text-green-400 transition" />
+            <FaInstagram className="hover:text-green-400 transition" />
+            <FaLinkedin className="hover:text-green-400 transition" />
+            <FaFacebookF className="hover:text-green-400 transition" />
         </div>
 
         {/* Mobile Menu Button */}
